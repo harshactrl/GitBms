12e89699da259f32e9120e3f165e11054c28ad93
Git_Srikanth <harshactrl@gmail.com>
2021-01-20 00:31:42 +0530

    JIRA:120 comapare to string

--- src/com/java/StringsToLearn.java
+++ src/com/java/StringsToLearn.java
@@ -3,9 +3,13 @@ package com.java;
 public class StringsToLearn {
 
 	public static void main(String[] args) {
-
-		System.out.println("Demo git project for string");
-		
+		String str1 ="Raj";
+		String str2 = new String("Raj");
+		if(str1==str2) {
+			System.out.println("true");
+		}
+		else {
+			System.out.println("false");
+		}
 	}
-
 }
